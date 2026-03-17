fb529f7dfee22b9802c7ca9b480306511e3a363c
Iarly Mariano <marianoiarly@gmail.com>
2026-03-17 11:42:18 -0300

    q7

--- ed/gomos/go/draft.go
+++ ed/gomos/go/draft.go
@@ -1,5 +1,40 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+	var q int
+	var d string
+	fmt.Scan(&q, &d)
+	x := make([]int, q)
+	y := make([]int, q)
+
+	for i := 0; i < q; i++ {
+		fmt.Scan(&x[i], &y[i])
+	}
+	antigax := x[0]
+	antigay := y[0]
+
+	if d == "L" {
+		x[0]--
+	} else if d == "R" {
+		x[0]++
+	} else if d == "U" {
+		y[0]--
+	} else if d == "D" {
+		y[0]++
+	}
+	for i := 1; i < q; i++ {
+		atualx := x[i]
+		atualy := y[i]
+
+		x[i] = antigax
+		y[i] = antigay
+
+		antigax = atualx
+		antigay = atualy
+	}
+	for i := 0; i < q; i++ {
+		fmt.Println(x[i], y[i])
+	}
 }
