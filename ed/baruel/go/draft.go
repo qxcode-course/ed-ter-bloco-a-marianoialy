7663326496ab26ea98413a326c2e485fdb876a3b
Iarly Mariano <marianoiarly@gmail.com>
2026-03-17 11:13:35 -0300

    q6

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -8,8 +8,8 @@ func main() {
 	fmt.Scan(&quantfig)
 	fmt.Scan(&quantpossui)
 	vetor := make([]int, quantpossui)
-	var repetidas int
-	var coladas int
+	repetidas := make([]int, quantpossui)
+	var quantrep int
 	for i := 0; i < quantpossui; i++ {
 		fmt.Scan(&vetor[i])
 	}
@@ -17,14 +17,50 @@ func main() {
 		if vetor[i] == 0 {
 			continue
 		}
-		for j := 0; j < quantpossui; j++ {
+		for j := i + 1; j < quantpossui; j++ {
 			if vetor[i] == vetor[j] {
-				repetidas++
+				repetidas[quantrep] = vetor[j]
+				quantrep++
 				vetor[j] = 0
 			}
 		}
-		coladas++
 	}
-	fmt.Println(repetidas)
-	fmt.Println(quantfig - coladas)
+	if quantrep == 0 {
+		fmt.Println("N")
+	} else {
+		for i := 0; i < quantrep; i++ {
+			if i > 0 {
+				fmt.Print(" ")
+			}
+			fmt.Print(repetidas[i])
+		}
+		fmt.Println()
+	}
+
+	faltantes := make([]int, quantfig)
+	quantfalt := 0
+	for i := 1; i <= quantfig; i++ {
+		count := 0
+		for j := 0; j < quantpossui; j++ {
+			if vetor[j] == i {
+				count++
+				break
+			}
+		}
+		if count == 0 {
+			faltantes[quantfalt] = i
+			quantfalt++
+		}
+	}
+	if quantfalt == 0 {
+		fmt.Println("N")
+	} else {
+		for i := 0; i < quantfalt; i++ {
+			if i > 0 {
+				fmt.Print(" ")
+			}
+			fmt.Print(faltantes[i])
+		}
+		fmt.Println()
+	}
 }
