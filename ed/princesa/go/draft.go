843ebee62ca6eb57537bc0bfd17658a858b23fd8
Iarly Mariano <marianoiarly@gmail.com>
2026-03-30 16:07:36 -0300

    att

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -4,11 +4,44 @@ import "fmt"
 
 func main() {
 	var n int
-	fmt.Scan(&n)
 	var e int
-	fmt.Scan(&e)
+	fmt.Scan(&n, &e)
 	vetor := make([]int, n)
 	for i := 0; i < n; i++ {
 		vetor[i] = i + 1
+	}
+	pos := e - 1
+	vivos := n
+
+	for vivos > 0 {
+		fmt.Print("[ ")
+		for i := 0; i < n; i++ {
+			if vetor[i] != 0 {
+				if i == pos {
+					fmt.Printf("%d> ", vetor[i])
+				} else {
+					fmt.Printf("%d ", vetor[i])
+				}
+			}
+		}
+		fmt.Println("]")
+		if vivos == 1 {
+			break
+		}
+		for {
+			posMorto := (pos + 1) % n
+			if vetor[posMorto] != 0 {
+				vetor[posMorto] = 0
+				vivos--
+				break
+			}
+			pos++
+		}
+		for {
+			pos = (pos + 1) % n
+			if vetor[pos] != 0 {
+				break
+			}
+		}
 	}
 }
