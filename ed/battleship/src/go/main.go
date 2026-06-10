4573e289a213f78bcd36b944581a72fbe04d1745
Iarly Mariano <marianoiarly@gmail.com>
2026-06-10 19:49:48 -0300

    att

--- ed/battleship/src/go/main.go
+++ ed/battleship/src/go/main.go
@@ -6,11 +6,32 @@ import (
 	"os"
 )
 
+func dfs(matriz [][]byte, i, j int) {
+	if i < 0 || i >= len(matriz) || j < 0 || j >= len(matriz[0]) {
+		return
+	}
+	if matriz[i][j] != 'X' {
+		return
+	}
+	matriz[i][j] = '.'
+	dfs(matriz, i+1, j)
+	dfs(matriz, i-1, j)
+	dfs(matriz, i, j+1)
+	dfs(matriz, i, j-1)
+}
+
 // Função que será chamada no LeetCode
 func countBattleships(board [][]byte) int {
-	//
-	_ := board
-	return 0
+	cont := 0
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			if board[i][j] == 'X' {
+				cont++
+				dfs(board, i, j)
+			}
+		}
+	}
+	return cont
 }
 
 // Não modifique a função main
