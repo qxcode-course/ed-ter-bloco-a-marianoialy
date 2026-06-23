2f4ba14315a39ed3348bb71ae06e18864034133d
Iarly Mariano <marianoiarly@gmail.com>
2026-06-23 10:54:16 -0300

    att

--- ed/queimando/src/go/main.go
+++ ed/queimando/src/go/main.go
@@ -6,18 +6,39 @@ import (
 	"os"
 )
 
+type Pos struct {
+	l, c int
+}
+
 func burnTrees(grid [][]rune, l, c int) {
 	stack := NewStack[Pos]()
-	_ , _ , _ = mat, l, c
+	stack.Push(Pos{l, c})
+	for !stack.IsEmpty() {
+		p := stack.Pop()
+		linha := p.l
+		coluna := p.c
 
-	// Essa função deve usar uma list como pilha
-	// e marcar as árvores na matriz como queimados
-	// Uma sugestão de como fazer isso é:
-	// - adicionar a primeira posição na pilha
-	// - enquanto a pilha não estiver vazia:
-	//   - retirar o elemento do topo
-	//   - se puder ser queimado, queime e adicione seus vizinhos à pilha
+		if linha < 0 || linha >= len(grid) || coluna < 0 || coluna >= len(grid[0]) {
+			continue
+		}
 
+		if grid[linha][coluna] != '#' {
+			continue
+		}
+		grid[linha][coluna] = 'o'
+
+		stack.Push(Pos{linha - 1, coluna})
+		stack.Push(Pos{linha + 1, coluna})
+		stack.Push(Pos{linha, coluna - 1})
+		stack.Push(Pos{linha, coluna + 1})
+		// Essa função deve usar uma list como pilha
+		// e marcar as árvores na matriz como queimados
+		// Uma sugestão de como fazer isso é:
+		// - adicionar a primeira posição na pilha
+		// - enquanto a pilha não estiver vazia:
+		//   - retirar o elemento do topo
+		//   - se puder ser queimado, queime e adicione seus vizinhos à pilha
+	}
 }
 
 func main() {
